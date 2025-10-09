06868f27541b53264dcb0ab5655a105c0505e221
Yuri Morozov <moroz69off@users.noreply.github.com>
2025-10-09 08:37:22 +0300

    Update index.js
    get page size

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
 // mnemoschema
-
+const pageWidth = document.documentElement.scrollWidth
+const pageHeight = document.documentElement.scrollHeight
 let _ALL = document.getElementById("__all");
