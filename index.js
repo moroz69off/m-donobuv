109742f8ec5d35ff3baa5c96102e3afbdb6069a0
Yuri Morozov <moroz69off@users.noreply.github.com>
2026-01-30 15:37:38 +0300

    Update index.js
    view sizes

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 const pageWidth = document.documentElement.scrollWidth
 const pageHeight = document.documentElement.scrollHeight
 let _ALL = document.getElementById("__all");
+
+console.log(pageWidth);
+console.log(pageHeight);
