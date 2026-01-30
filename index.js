afe23a5d1373880cf010d937f45185e5672212bf
Yuri Morozov <moroz69off@users.noreply.github.com>
2026-01-30 16:05:06 +0300

    Update index.js
    get canvas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const pageWidth = document.documentElement.scrollWidth
 const pageHeight = document.documentElement.scrollHeight
 let _ALL = document.getElementById("__all");
+let canvas = document.getElementById("canvas");
+console.log(canvas.width);
 
 console.log(pageWidth);
 console.log(pageHeight);
