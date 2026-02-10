1b92b203aa14671aa4557c1550b6621a104ac6bd
moroz69off <moroz69off@gmail.com>
2026-02-10 15:57:58 +0300

    canvas problem

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const pageWidth = document.documentElement.scrollWidth
 const pageHeight = document.documentElement.scrollHeight
 const _ALL = document.getElementById("__all");
+
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
 
@@ -11,11 +12,12 @@ window.addEventListener('resize', function() {
     console.log('Window resized!');
     // Perform actions here, like adjusting layouts or recalculating sizes
     const width = window.innerWidth;
-    const height = window.innerHeight;
+    //const height = window.innerHeight;
+	const height = pageHeight;
     console.log(`New size: ${width}px x ${height}px`);
 
-	canvas.width = pageWidth;
-	canvas.height = pageHeight;
+	canvas.width = width;
+	canvas.height = height;
 
 });
 
